9cf4a051b55ba2588f0876b87dd2f26904ec21ad
JR Strayhorn <jrstrayhorn@gmail.com>
2021-05-06 17:33:48 -0400

    done - route params

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 // 'coffees' ties route /coffees to this controller
 // still need HTTP verb method
@@ -7,5 +7,13 @@ export class CoffeesController {
   @Get('flavors')
   findAll() {
     return 'this action returns all coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    // just @Param() params gives us all items in request parameters
+    // so we'd need to do params.id to get value
+    // give value in @Param() to get specific parameter
+    return `This action returns ${id} coffee`;
   }
 }

50732be3278669cbf0e8276f9822393d30853f45
JR Strayhorn <jrstrayhorn@gmail.com>
2021-05-06 18:58:52 -0400

    wip - rest api

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesController } from './coffees/coffees.controller';
+import { CoffeesService } from './coffees/coffees.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CoffeesController],
-  providers: [AppService],
+  // a provider can inject dependencies
+  // objects create relationships with each other
+  // NEST handles DI for us
+  providers: [AppService, CoffeesService],
 })
 export class AppModule {}
--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,12 +1,38 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+  Res,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 // 'coffees' ties route /coffees to this controller
 // still need HTTP verb method
 @Controller('coffees')
 export class CoffeesController {
-  @Get('flavors')
-  findAll() {
-    return 'this action returns all coffees';
+  // declare and init coffessService and keep scope to class only w/ private
+  // readonly so we won't be able to change it
+  constructor(private readonly coffeesService: CoffeesService) {}
+  @Get()
+  findAll(@Query() paginationQuery) {
+    // findAll(@Res() response) {
+    // using the underlining response object from express or fastify
+    // provides great flexibility to mess with headers etc
+    // but in practice use with care b/c lose compat with nest features
+    // like interceptors.. our code becomes platform depending
+    // code is also harder to test
+    // best practice use nest standard approach
+    // response.status(200).send('this action returns all coffees');
+    const { limit, offset } = paginationQuery;
+    return this.coffeesService.findAll();
+    // return `This action returns all coffees. Limit: ${limit}, offset: ${offset}`;
   }
 
   @Get(':id')
@@ -14,6 +40,39 @@ export class CoffeesController {
     // just @Param() params gives us all items in request parameters
     // so we'd need to do params.id to get value
     // give value in @Param() to get specific parameter
-    return `This action returns ${id} coffee`;
+    // return `This action returns ${id} coffee`;
+    return this.coffeesService.findOne(id);
+  }
+
+  // @Post()
+  // @HttpCode(HttpStatus.GONE) // good for static status codes
+  // create(@Body() body) {
+  //   return body;
+  // }
+  @Post()
+  create(@Body() body) {
+    return this.coffeesService.create(body);
+  }
+
+  // posting and getting specific portion of body
+  // may have valuation issues so use this with caution
+  // because other items in body won't get validated
+  @Post('specBody')
+  createSpecBody(@Body('name') name: string) {
+    return name;
+  }
+
+  // put -  replaces entire resource - need whole object in request payload
+  // patch - can update specific portion of object - can pass small portion of whole object in request
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    // return `this action updates ${id} coffee`;
+    return this.coffeesService.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    // return `this action removes ${id} coffee`;
+    return this.coffeesService.remove(id);
   }
 }

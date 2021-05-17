6005a61fba92587a768bc2247763b75bfafeece8
JR Strayhorn <jrstrayhorn@gmail.com>
2021-05-17 07:33:52 -0400

    done - value based provider

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,12 +6,20 @@ import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
+class MockCoffeesService {}
+
 @Module({
   // registers TypeOrmModule in child feature
   // pass in array of entities
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    //CoffeesService
+    {
+      provide: CoffeesService,
+      useValue: new MockCoffeesService(),
+    },
+  ],
   exports: [CoffeesService],
 })
 export class CoffeesModule {}

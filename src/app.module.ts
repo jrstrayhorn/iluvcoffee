37677e425f734a2338e91c4abc44c2fff4e1aef5
JR Strayhorn <jrstrayhorn@gmail.com>
2021-05-19 19:07:30 -0400

    done - async config

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,25 @@ import appConfig from './config/app.config';
 
 @Module({
   imports: [
+    // this breaks b/c typeOrmModule is loading BEFORE process.env values
+    // order of imports should not matter at all.. how to fix use async
+    TypeOrmModule.forRootAsync({
+      // can inject configService if we want
+      // async factory is loaded AFTER the rest of core modules are loaded
+      useFactory: () => ({
+        type: 'mysql',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true, // load modules automatically w/o entities arries
+        synchronize: true,
+        // typeOrm will be synced each time, disable in production -
+        // auto generate table with any class with @Entity decorator
+        // this is for dev only
+      }),
+    }),
     // looks for .env file in root directory by default
     ConfigModule.forRoot({
       validationSchema: Joi.object({
@@ -22,19 +41,7 @@ import appConfig from './config/app.config';
       // ignoreEnvFile: true // may want to do this for production since going to read from hosting variables instead of env file
     }), // will read process env files and environment variables
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true, // load modules automatically w/o entities arries
-      synchronize: true,
-      // typeOrm will be synced each time, disable in production -
-      // auto generate table with any class with @Entity decorator
-      // this is for dev only
-    }),
+
     CoffeeRatingModule,
     DatabaseModule,
   ],

35b92e70e2b17ddc404007c2d71757b3a3cf2ac5
JR Strayhorn <jrstrayhorn@gmail.com>
2021-06-07 08:08:49 -0400

    fixing e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest'; // highlevel abstraction over http requests
+import * as request from 'supertest'; // highlevel abstraction for testing http requests
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
@@ -17,8 +17,9 @@ describe('AppController (e2e)', () => {
     await app.init(); // have to call this, to mount routes and lifecycle hooks
   });
 
+  // http request using supertest
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
+    return request(app.getHttpServer()) // passing reference to http listener either express or fastify
       .get('/')
       .expect(200)
       .expect('Hello World!');
